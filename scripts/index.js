fae30e3d11cfb19a1bd618820a98d0b4ac582f68
Daniil Molchanov <d.1.molchanov@yandex.ru>
2024-06-23 11:38:46 +0300

    добавление карточек и удаление

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,19 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
+function createCard (information, deleteCard) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector(".places__item").cloneNode(true);
+  cardElement.querySelector(".card__image").src = information.link;
+  cardElement.querySelector(".card__title").textContent = information.name;
+  cardElement.querySelector(".card__delete-button");
+  cardElement.addEventListener("click", deleteCard);
+  return cardElement;
+};
 
-// @todo: Функция создания карточки
+const deleteCard = function (event) {
+  event.target.closest(".card").remove();
+}
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+const placesList = document.querySelector(".places__list");
+initialCards.forEach((cardInformation) => {
+  placesList.append(createCard(cardInformation, deleteCard));
+});
